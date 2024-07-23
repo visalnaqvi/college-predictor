17530e18b7cbaaabec310088d85c88203c482695
unknown <visalnaqvi@gmail.com>
2024-07-23 10:15:51 +0530

    first

---
+++ src/data/dara.js
@@ -0,0 +1,242 @@
+export default [{
+    college:"Kirori Mal College",
+    course:"B.A. (H) English",
+    ur:99,
+    obc:97,
+    sc:96,
+    st:95,
+    ews:98.50
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (H) Hindi",
+    ur:95.50,
+    obc:95.25,
+    sc:94.50,
+    st:94,
+    ews:95
+},{
+    college:"Kirori Mal College",
+    course:"B.A. Urdu (H)",
+    ur:86,
+    obc:85,
+    sc:81,
+    st:80,
+    ews:85
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (H) Sanskrit",
+    ur:86,
+    obc:85,
+    sc:84,
+    st:83,
+    ews:85
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (H) Geography",
+    ur:100,
+    obc:99,
+    sc:98.50,
+    st:98,
+    ews:99
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (II) Political Science",
+    ur:100,
+    obc:100,
+    sc:99,
+    st:99,
+    ews:100
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (H) History",
+    ur:99,
+    obc:98,
+    sc:97,
+    st:96,
+    ews:98
+},{
+    college:"Kirori Mal College",
+    course:"B.A. (H) Economics",
+    ur:100,
+    obc:99,
+    sc:99,
+    st:97.50,
+    ews:99
+},{
+    college:"Kirori Mal College",
+    course:"B.Com. (H)",
+    ur:100,
+    obc:100,
+    sc:99,
+    st:98.50,
+    ews:100
+},{
+    college:"Kirori Mal College",
+    course:"B.Com.",
+    ur:100,
+    obc:100,
+    sc:99,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (Prog.) Category A*",
+    ur:99,
+    obc:98,
+    sc:98,
+    st:96,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (Prog.) Category B*",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (Prog.) Category C*",
+    ur:100,
+    obc:100,
+    sc:100,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) Economics",
+    ur:100,
+    obc:100,
+    sc:100,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) English",
+    ur:100,
+    obc:100,
+    sc:100,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) Hindi",
+    ur:96,
+    obc:93,
+    sc:90,
+    st:88,
+    ews:96
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) History",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) Political Science",
+    ur:100,
+    obc:100,
+    sc:100,
+    st:100,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.A. (H) Sanskrit",
+    ur:81,
+    obc:79,
+    sc:78,
+    st:72,
+    ews:80
+},{
+    college:"Ramjas College",
+    course:"B.Com",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Com (H)",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Botany",
+    ur:98,
+    obc:98,
+    sc:93,
+    st:93,
+    ews:98
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Chemistry",
+    ur:100,
+    obc:100,
+    sc:95,
+    st:86,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Mathematics",
+    ur:100,
+    obc:98,
+    sc:98,
+    st:98,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Physics",
+    ur:100,
+    obc:100,
+    sc:94,
+    st:85,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Statistics",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:94,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. (H) Zoology",
+    ur:100,
+    obc:100,
+    sc:98,
+    st:94,
+    ews:100
+},{
+    college:"Ramjas College",
+    course:"B.Sc. Life Sciences",
+    ur:98,
+    obc:97,
+    sc:94,
+    st:91,
+    ews:98
+},{
+    college:"Ramjas College",
+    course:"B.Sc. Physical Science with Chemistry",
+    ur:98,
+    obc:98,
+    sc:94,
+    st:86,
+    ews:98
+},{
+    college:"Ramjas College",
+    course:"B.Sc. Physical Science with Chemistry",
+    ur:100,
+    obc:98,
+    sc:94,
+    st:86,
+    ews:98
+}
+]
